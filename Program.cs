1de7f65dbedef79b1a2b3aeb85a017b95a668e6f
peanut-lover <jihoson94@gmail.com>
2024-01-27 10:43:26 +0900

    Init Project

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+// Define dynamic module
+var assemblyName = new AssemblyName("ExampleDynamicAssembly");
+var dynamicAssembly = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+var dynamicModule = dynamicAssembly.DefineDynamicModule("ExampleDynamicModule");
